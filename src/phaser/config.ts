71ef22267ac529e30f925786f3007e8368d6da96
sebsowter <seb@sebsowter.com>
2024-12-29 13:11:55 +0000

    Refactor

--- src/phaser/config.ts
+++ src/phaser/config.ts
@@ -2,7 +2,7 @@ import { BootScene, GameScene, LoaderScene } from "./scenes";
 
 export const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 640,
+  width: 320,
   height: 320,
   scene: [BootScene, LoaderScene, GameScene],
 };
